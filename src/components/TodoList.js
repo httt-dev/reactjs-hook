c6bc7d8da83d2109a0fac0d6e69359b4c7ff31af
Hoa Nguyen <httt.dev@gmail.com>
2022-05-15 11:48:52 +0700

    09_Dom events

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,13 +4,17 @@ const TodoList = () => {
     //khai bao state va khoi tao gia tri cho state
     //useState : tra ve 2 gia tri 
     const [name, setName] = useState("Hoa")
-
+    const handleOnClickBtn = (event, data) => {
+        console.log("Click event", data);
+    }
     return (
         <div>
             <label>Name</label>
             <input type="text" value={name} onChange={(data) => setName(data.target.value)} />
             <br></br>
             Todo list ... name ={name}
+            <button type="button" onClick={(env) => handleOnClickBtn(env, 'data')}>Submit</button>
+            {/* <button type="button" onClick={handleOnClickBtn}>Submit</button> */}
         </div>
     );
 }
