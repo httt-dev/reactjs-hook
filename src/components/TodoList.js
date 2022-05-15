f8f649618ff375414873b57179564d275f50ae96
Hoa Nguyen <httt.dev@gmail.com>
2022-05-15 11:21:34 +0700

    #08 Dynamic Values & UseState Hook - Sử dụng biến với React & useState Hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// const TodoList = () => {
+const TodoList = () => {
+    //khai bao state va khoi tao gia tri cho state
+    //useState : tra ve 2 gia tri 
+    const [name, setName] = useState("Hoa")
 
-//     return (
-//         <div>
-//             Todo List
-//         </div>
-//     );
-// }
-
-class TodoList extends React.Component {
-    state = {
-        name: 'Hoa'
-    }
-
-    render() {
-        return (
-            <div>
-                <label>Name</label>
-                <input type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
-                <br></br>
-                Todo list with name = {this.state.name}
-            </div>
-
-        )
-    }
+    return (
+        <div>
+            <label>Name</label>
+            <input type="text" value={name} onChange={(data) => setName(data.target.value)} />
+            <br></br>
+            Todo list ... name ={name}
+        </div>
+    );
 }
+
+// class TodoList extends React.Component {
+//     state = {
+//         name: 'Hoa'
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <label>Name</label>
+//                 <input type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
+//                 <br></br>
+//                 Todo list with name = {this.state.name}
+//             </div>
+
+//         )
+//     }
+// }
 export default TodoList;
